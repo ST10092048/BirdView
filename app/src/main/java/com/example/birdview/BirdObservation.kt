43460e49e50af6a37ff21daf5c164969fc84a8f3
ST10092048 <neonkgabutle81@gmail.com>
2023-09-05 03:12:19 +0200

    The project has just started

---
+++ app/src/main/java/com/example/birdview/BirdObservation.kt
@@ -0,0 +1,11 @@
+package com.example.birdview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BirdObservation : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bird_observation)
+    }
+}
